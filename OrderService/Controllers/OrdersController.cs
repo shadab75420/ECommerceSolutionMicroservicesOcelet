576de3dcb4f9d995fe55a9247f16c987c2aea431
shadab <shadabbgs3@gmail.com>
2026-05-25 21:00:31 +0530

    Add project files.

---
+++ OrderService/Controllers/OrdersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetOrders()
+        {
+            var orders = new List<string>
+            {
+                "Order 101",
+                "Order 102",
+                "Order 103"
+            };
+
+            return Ok(orders);
+        }
+    }
+}
---
+++ ProductService/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = new List<string>
+            {
+                "Laptop",
+                "Mobile",
+                "Keyboard"
+            };
+
+            return Ok(products);
+        }
+    }
+}
